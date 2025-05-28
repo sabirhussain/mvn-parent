9eed4a953d24afce0fd3b1767ae358adde2d080b
Hussain <sabir.c.hussain@rakuten.com>
2025-05-28 12:38:22 +0900

    initial commit

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export IMAGE_REGISTRY=https://hub.docker.com/repository/docker
+export IMAGE_REPO=rmc-cx
+export SONAR_PROJECT_KEY=SONAR-KEY
+export SONAR_MODULE_KEY=rmc-cx
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Set build environment
+```bash
+$ source env.sh
+```
